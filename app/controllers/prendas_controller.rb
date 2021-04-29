e86b07a6a1f8e93b147f3ce51737fe83cb717823
Gonzalo Heredia <lalo1heredia@gmail.com>
2021-04-29 01:48:26 -0300

    Adiciones clase 29/04

--- app/controllers/prendas_controller.rb
+++ app/controllers/prendas_controller.rb
@@ -1,8 +1,34 @@
 class PrendasController < ApplicationController
+  
   def index
     @prendas = Prenda.all
   end
 
   def show
+    @prendas = Prenda.find(params[:id])
+  end
+
+  def create
+    Prenda.create! prenda_params
+    redirect_to action: :index
+  end
+
+  def new
+    @prenda = Prenda.new
+  end
+
+  def update
+    @prenda = Prenda.find(params[:id])
+    @prenda.update! prenda_params
+    redirect_to @prenda
+  end
+
+  def edit
+    @prenda = Prenda.find(params[:id])
+  end
+
+  private
+  def prenda_params
+    params.require(:prenda).permit(:tipo, :categoria)
   end
 end
--- app/models/prenda.rb
+++ app/models/prenda.rb
@@ -1,3 +1,3 @@
 class Prenda < ApplicationRecord
-    enum categoria: [:torso, :piernas, :pies]
+    enum categoria: [:torso, :piernas, :pies, :accesorio]
 end

b0408215409bc8bea9a49d532e5b43f9f50b2b6d
Lucas <lucasagustininfantino@hotmail.com>
2021-05-19 22:43:05 -0300

    clima y cambios a navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,32 @@
 //= require turbolinks
 //= require_tree ./application
 
+var xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=Buenos Aires&appid=c2e8f8aee7f46d429a8d82dd755b236b&lang=es&units=metric', true);
+
+xhr.onload = function(){
+    document.getElementById('clima-loader').hidden = true;
+    if(this.status == 200){
+        let climaResponse = JSON.parse(this.responseText);
+        
+        let climaDescription = climaResponse.weather[0].description + ", ";
+        climaDescription += climaResponse.main.temp + "°C";
+        let climaIcon = climaResponse.weather[0].icon;
+        let climaHTML = `<span><img class = "nav-img" src = "https://raw.githubusercontent.com/yuvraaaj/openweathermap-api-icons/master/icons/${climaIcon}.png"></img> ${climaDescription}</span>`
+        document.getElementById('clima-container').innerHTML = climaHTML;
+        //<i class = "${climaIcon}"></i>
+    }
+}
+
+xhr.onprogress = function(){
+    document.getElementById('clima-loader').hidden = false;
+}
+
+
+xhr.onerror = function(){
+    document.getElementById('clima-loader').hidden = true;
+    console.log('Request Error...');
+}
+
+xhr.send();

9d3a9e97fe9437f5fd1c373526eedb046ad80216
fedex3 <federicoianamor@gmail.com>
2021-05-23 04:48:20 -0300

    Selector de prendas para atuendos con Javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,35 +14,117 @@
 
 //= require_tree ./application
 
-var xhr = new XMLHttpRequest();
+document.addEventListener("load", ClimaYVariables);
+function ClimaYVariables()
+{
 
-xhr.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=Buenos Aires&appid=c2e8f8aee7f46d429a8d82dd755b236b&lang=es&units=metric', true);
 
-xhr.onload = function(){
-    document.getElementById('clima-loader').hidden = true;
-    if(this.status == 200){
-        let climaResponse = JSON.parse(this.responseText);
-        
-        let climaDescription = climaResponse.weather[0].description + ", ";
-        climaDescription += climaResponse.main.temp + "°C";
-        let climaIcon = climaResponse.weather[0].icon;
-        let climaHTML = `<span><img class = "nav-img" src = "https://raw.githubusercontent.com/yuvraaaj/openweathermap-api-icons/master/icons/${climaIcon}.png"></img> ${climaDescription}</span>`
-        document.getElementById('clima-container').innerHTML = climaHTML;
-        //<i class = "${climaIcon}"></i>
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=Buenos Aires&appid=c2e8f8aee7f46d429a8d82dd755b236b&lang=es&units=metric', true);
+    
+    xhr.onload = function(){
+        document.getElementById('clima-loader').hidden = true;
+        if(this.status == 200){
+            let climaResponse = JSON.parse(this.responseText);
+            
+            let climaDescription = climaResponse.weather[0].description + ", ";
+            climaDescription += climaResponse.main.temp + "°C";
+            let climaIcon = climaResponse.weather[0].icon;
+            let climaHTML = `<span><img class = "nav-img" src = "https://raw.githubusercontent.com/yuvraaaj/openweathermap-api-icons/master/icons/${climaIcon}.png"></img> ${climaDescription}</span>`
+            document.getElementById('clima-container').innerHTML = climaHTML;
+            //<i class = "${climaIcon}"></i>
+        }
     }
+    
+    xhr.send();
+
+    
 }
 
-xhr.send();
+document.addEventListener("DOMContentLoaded", theDomHasLoaded);
 
-//var imagenes_atuendos_new = document.getElementId('card_atuendos_new');
-//
-//imagenes_atuendos_new.onclick = function(){
-//    imagenes_atuendos_new.style.border = "5px solid #FFFFFF";
-//}
+var atuendos_new
 
-//function seleccionar() {
-//    var imagenes_atuendos_new = document.getElementId('card_atuendos_new');
-//    imagenes_atuendos_new.style.border = "5px solid #FFFFFF";
-//}
+function theDomHasLoaded() 
+{
+    atuendos_new_torso = document.querySelectorAll(".card_atuendos_new_torso");
+    atuendos_new_piernas = document.querySelectorAll(".card_atuendos_new_piernas");
+    atuendos_new_pies = document.querySelectorAll(".card_atuendos_new_pies");
+    atuendos_new_accesorio = document.querySelectorAll(".card_atuendos_new_accesorio");
+
+
+    Seleccionar_Torso()
+    Seleccionar_Piernas()
+    Seleccionar_Pies()
+    Seleccionar_Accesorio()
+}
+
+function Seleccionar_Torso()
+{
+    atuendos_new_torso.forEach(Node => {
+        Node.addEventListener("click",  ()=>{  
+            Node.style.border = "thick double #1B8F88" 
+            for(var i = 0; i < atuendos_new_torso.length; i++)
+            {
+                if(atuendos_new_torso[i] != Node)
+                {
+                    atuendos_new_torso[i].style.border = ""
+                }
+            }
+        })
+    });
+}
+
+function Seleccionar_Piernas()
+{
+    atuendos_new_piernas.forEach(Node => {
+        Node.addEventListener("click",  ()=>{  
+            Node.style.border = "thick double #1B8F88" 
+            for(var i = 0; i < atuendos_new_piernas.length; i++)
+            {
+                if(atuendos_new_piernas[i] != Node)
+                {
+                    atuendos_new_piernas[i].style.border = ""
+                }
+            }
+        })
+    });
+}
+
+function Seleccionar_Pies()
+{
+    atuendos_new_pies.forEach(Node => {
+        Node.addEventListener("click",  ()=>{  
+            Node.style.border = "thick double #1B8F88" 
+            for(var i = 0; i < atuendos_new_pies.length; i++)
+            {
+                if(atuendos_new_pies[i] != Node)
+                {
+                    atuendos_new_pies[i].style.border = ""
+                }
+            }
+        })
+    });
+}
+
+function Seleccionar_Accesorio()
+{
+    atuendos_new_accesorio.forEach(Node => {
+        Node.addEventListener("click",  ()=>{  
+            Node.style.border = "thick double #1B8F88" 
+            for(var i = 0; i < atuendos_new_accesorio.length; i++)
+            {
+                if(atuendos_new_accesorio[i] != Node)
+                {
+                    atuendos_new_accesorio[i].style.border = ""
+                }
+            }
+        })
+    });
+}
+
+
+
 
 //var confirmar_volver = prompt("Seguro que desea volver sin guardar?");

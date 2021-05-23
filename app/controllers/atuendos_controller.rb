c9d0efc323918ba947925a9bf116c9cef6d031ae
Lucas <lucasagustininfantino@hotmail.com>
2021-05-23 18:09:58 -0300

    despues de mucho sufrimiento esta el create de los atuendos

--- app/controllers/atuendos_controller.rb
+++ app/controllers/atuendos_controller.rb
@@ -12,7 +12,7 @@ class AtuendosController < ApplicationController
     end
 
     def show
-        @atuendo = atuendo.find(params[:id])
+        @atuendo = Atuendo.find(params[:id])
         #if !@atuendo.color_secundario.blank? 
         #    if !@atuendo.mismo_color?
         #    @atuendo.color_primario = @atuendo.color_primario + " y " + @atuendo.color_secundario
@@ -25,48 +25,85 @@ class AtuendosController < ApplicationController
         @atuendo = @guardarropa.atuendos.build (atuendo_params)
         #@atuendo.comprobar_color_secundario
         @atuendo.save
-        redirect_to guardarropa_atuendo_path(@guardarropa,@atuendo)
+        redirect_to guardarropa_atuendos_path(@guardarropa)
     end
 
     def new
         @atuendo = @guardarropa.atuendos.build
+        @i = 0
 
         @prendas_torso = @guardarropa.prendas.select{|p| p.categoria == "Torso"}
-        @prendas_torso_paginable = Kaminari.paginate_array(@prendas_torso).page(params[:torso_page]).per(4)
+        #@prendas_torso_paginable = Kaminari.paginate_array(@prendas_torso).page(params[:torso_page]).per(4)
+        @prenda_torso = @prendas_torso[rand(@prendas_torso.length)]
+        @conversion_torso = Array.new
+
+        @prendas_torso.each do |prenda|
+            @conversion_torso[@i] = [prenda.tipo,prenda.id]
+            @i+=1
+        end
+
+        @i=0
 
         @prendas_piernas = @guardarropa.prendas.select{|p| p.categoria == "Piernas"}
-        @prendas_piernas_paginable = Kaminari.paginate_array(@prendas_piernas).page(params[:piernas_page]).per(4)
+        #@prendas_piernas_paginable = Kaminari.paginate_array(@prendas_piernas).page(params[:piernas_page]).per(4)
+        @prenda_piernas = @prendas_piernas[rand(@prendas_piernas.length)]
+        @conversion_piernas = Array.new
+
+        @prendas_piernas.each do |prenda|
+            @conversion_piernas[@i] = [prenda.tipo,prenda.id]
+            @i+=1
+        end
+
+        @i=0
 
         @prendas_pies = @guardarropa.prendas.select{|p| p.categoria == "Pies"}
-        @prendas_pies_paginable = Kaminari.paginate_array(@prendas_pies).page(params[:pies_page]).per(4)
+        #@prendas_pies_paginable = Kaminari.paginate_array(@prendas_pies).page(params[:pies_page]).per(4)
+        @prenda_pies = @prendas_pies[rand(@prendas_pies.length)]
+        @conversion_pies = Array.new
 
-        @prendas_accesorio = @guardarropa.prendas.select{|p| p.categoria == "Accesorio"}
-        @prendas_accesorio_paginable = Kaminari.paginate_array(@prendas_accesorio).page(params[:accesorio_page]).per(4)
+        @prendas_pies.each do |prenda|
+            @conversion_pies[@i] = [prenda.tipo,prenda.id]
+            @i+=1
+        end
+
+        @i=0
+
+        @prendas_accesorios = @guardarropa.prendas.select{|p| p.categoria == "Accesorio"}
+        #@prendas_accesorios_paginable = Kaminari.paginate_array(@prendas_accesorios).page(params[:accesorio_page]).per(4)
+        @prenda_accesorios = @prendas_accesorios[rand(@prendas_accesorios.length)]
+        @conversion_accesorios = Array.new
+
+        @prendas_accesorios.each do |prenda|
+            @conversion_accesorios[@i] = [prenda.tipo,prenda.id]
+            @i+=1
+        end
+
+        @i=0
 
         #@atuendo.prenda_torso = @prendas_torso[rand(@prendas_torso.count)]
     end
 
     def update
-        @atuendo = atuendo.find(params[:id])
+        @atuendo = Atuendo.find(params[:id])
         @atuendo.update! atuendo_params
         #@atuendo.comprobar_color_secundario
         #redirect_to guardarropa_atuendo_path(@guardarropa,@atuendo)
     end
 
     def edit
-        @atuendo = atuendo.find(params[:id])
+        @atuendo = Atuendo.find(params[:id])
         #@atuendo.comprobar_link_imagen!
     end
 
     def destroy
-        @atuendo = atuendo.find(params[:id])
+        @atuendo = Atuendo.find(params[:id])
         @atuendo.destroy!
         redirect_to action: :index
     end
 
     private
     def atuendo_params
-        params.require(:atuendo).permit(:guardarropa_id)#:tipo, :categoria, :tela, :color_primario, :color_secundario, :link_imagen, :imagen, 
+        params.require(:atuendo).permit(:Descripcion, :Puntaje, :Etiqueta_DiaNoche, :Etiqueta_Estacion, :Etiqueta_Formal, :guardarropa_id, :prenda_accesorios_id, :prenda_piernas_id, :prenda_pies_id, :prenda_torso_id)#:tipo, :categoria, :tela, :color_primario, :color_secundario, :link_imagen, :imagen, 
     end
 
     def get_guardarropa
--- app/models/atuendo.rb
+++ app/models/atuendo.rb
@@ -1,15 +1,6 @@
 class Atuendo < ApplicationRecord
-    #has_and_belongs_to_many :prenda
-    #validates_length_of :prendas, maximum: 4
-
     belongs_to :prenda_torso, class_name: "Prenda" 
-    #has_one :prenda_torso,  class_name: "Prenda"
-    #has_one :prenda_piernas,  class_name: "Prenda"
-    #has_one :prenda_pies,  class_name: "Prenda"
-    #has_one :prenda_accesorios,  class_name: "Prenda"
-    
-
-    #has_one :prenda, -> { where(categoria: 2) } #Piernas
-    #has_one :prenda, -> { where(categoria: 3) } #Pies
-    #has_one :prenda, -> { where(categoria: 4) } #Accesorios
+    belongs_to :prenda_piernas, class_name: "Prenda" 
+    belongs_to :prenda_pies, class_name: "Prenda" 
+    belongs_to :prenda_accesorios, class_name: "Prenda" 
 end

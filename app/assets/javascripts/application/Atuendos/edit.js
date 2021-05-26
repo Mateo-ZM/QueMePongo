b8cca1e5bd1c7bfee8166a001a27f5596e0da688
Mateo Zanetti Mendez <mateozanettim@gmail.com>
2021-05-25 21:34:20 -0300

    Iconos botones form atuendos, mejora visual select atuendo form

--- app/assets/javascripts/application/Atuendos/edit.js
+++ app/assets/javascripts/application/Atuendos/edit.js
@@ -15,9 +15,9 @@ Paloma.controller('Atuendos', {
             let id_torso_inicial = document.querySelectorAll('div.prenda_torso_inicial')
             let cadena_inicial_torso = "<option value= " + id_torso_inicial[0].innerHTML +"></option>"
             atuendos_new_torso[0].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.nextElementSibling.children[0].innerHTML = cadena_inicial_torso;
-            //id_torso_inicial[0].parentElement.parentElement.style.border = "thick double #1B8F88" 
+            //id_torso_inicial[0].parentElement.parentElement.style.border = "0.5rem solid  #1B8F88" 
             id_torso_inicial.forEach(Node => {
-                Node.parentElement.parentElement.style.border = "thick double #1B8F88"
+                Node.parentElement.parentElement.style.border = "0.5rem solid  #1B8F88"
             })
             
             //Inicializo piernas
@@ -26,7 +26,7 @@ Paloma.controller('Atuendos', {
             let cadena_inicial_piernas = "<option value= " + id_piernas_inicial[0].innerHTML +"></option>"
             atuendos_new_piernas[0].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.nextElementSibling.children[0].innerHTML = cadena_inicial_piernas;
             id_piernas_inicial.forEach(Node => {
-                Node.parentElement.parentElement.style.border = "thick double #1B8F88"
+                Node.parentElement.parentElement.style.border = "0.5rem solid  #1B8F88"
             }) 
 
             //Inicializo pies
@@ -35,7 +35,7 @@ Paloma.controller('Atuendos', {
             let cadena_inicial_pies = "<option value= " + id_pies_inicial[0].innerHTML +"></option>"
             atuendos_new_pies[0].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.nextElementSibling.children[0].innerHTML = cadena_inicial_pies;
             id_pies_inicial.forEach(Node => {
-                Node.parentElement.parentElement.style.border = "thick double #1B8F88"
+                Node.parentElement.parentElement.style.border = "0.5rem solid  #1B8F88"
             }) 
 
             //Inicializo accesorio
@@ -44,7 +44,7 @@ Paloma.controller('Atuendos', {
             let cadena_inicial_accesorio = "<option value= " + id_accesorio_inicial[0].innerHTML +"></option>"
             atuendos_new_accesorio[0].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.nextElementSibling.children[0].innerHTML = cadena_inicial_accesorio;
             id_accesorio_inicial.forEach(Node => {
-                Node.parentElement.parentElement.style.border = "thick double #1B8F88"
+                Node.parentElement.parentElement.style.border = "0.5rem solid  #1B8F88"
             }) 
 
             let boton = document.querySelector('button.form-control');
@@ -73,7 +73,7 @@ Paloma.controller('Atuendos', {
         {
             atuendos_new_torso.forEach(Node => {
                 Node.addEventListener("click",  ()=>{  
-                    Node.style.border = "thick double #1B8F88" 
+                    Node.style.border = "0.5rem solid  #1B8F88" 
                     for(var i = 0; i < atuendos_new_torso.length; i++)
                     {
                         if(atuendos_new_torso[i] != Node)
@@ -97,7 +97,7 @@ Paloma.controller('Atuendos', {
         {
             atuendos_new_piernas.forEach(Node => {
                 Node.addEventListener("click",  ()=>{  
-                    Node.style.border = "thick double #1B8F88" 
+                    Node.style.border = "0.5rem solid  #1B8F88" 
                     for(var i = 0; i < atuendos_new_piernas.length; i++)
                     {
                         if(atuendos_new_piernas[i] != Node)
@@ -120,7 +120,7 @@ Paloma.controller('Atuendos', {
         {
             atuendos_new_pies.forEach(Node => {
                 Node.addEventListener("click",  ()=>{  
-                    Node.style.border = "thick double #1B8F88" 
+                    Node.style.border = "0.5rem solid  #1B8F88" 
                     for(var i = 0; i < atuendos_new_pies.length; i++)
                     {
                         if(atuendos_new_pies[i] != Node)
@@ -143,7 +143,7 @@ Paloma.controller('Atuendos', {
         {
             atuendos_new_accesorio.forEach(Node => {
                 Node.addEventListener("click",  ()=>{  
-                    Node.style.border = "thick double #1B8F88" 
+                    Node.style.border = "0.5rem solid  #1B8F88" 
                     for(var i = 0; i < atuendos_new_accesorio.length; i++)
                     {
                         if(atuendos_new_accesorio[i] != Node)

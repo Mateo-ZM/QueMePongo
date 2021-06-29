49ae5282f43e70dcfa4c7fd65657e0fc68b343eb
Lucas <lucasagustininfantino@hotmail.com>
2021-06-28 22:03:07 -0300

    varios cambios a la parte de idiomas e italiano funcionando

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module QueMePongo
     # Don't generate system test files.
     config.generators.system_tests = nil
 
-    config.i18n.available_locales = [:en, :es, :de]
+    config.i18n.available_locales = [:en, :es, :de, :it]
     config.i18n.default_locale = :es
     config.i18n.fallbacks = false
 

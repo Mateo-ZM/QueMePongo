7138e860bda437b68edc18cf05bcd3220884a52a
fedex3 <federicoianamor@gmail.com>
2021-06-30 05:34:34 -0300

    A ver si ahora anda el mailer en heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   host = 'quemepongo2021.herokuapp.com'
-  config.action_mailer.default_url_options = { :host => host, protocol: 'http' }
+  config.action_mailer.default_url_options = { :host => 'quemepongo2021.herokuapp.com', protocol: 'http' }
 
   
   # SMTP settings for gmail
